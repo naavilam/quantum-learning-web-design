a2c4e99c1c85c0ba98dc38287ba5f9c33dbc9ee2
Nara Avila <naraavilamoraes@gmail.com>
2025-06-11 14:29:29 -0300

    Update files

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -82,11 +82,11 @@ const HeroSection = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+{/*      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-purple-300 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-purple-500 rounded-full mt-2 animate-pulse"></div>
         </div>
-      </div>
+      </div>*/}
     </section>
   );
 };
